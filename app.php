7147f9d631d984b8641ab0b160e312363296a50a
Surmon <surmon@foxmail.com>
2024-07-10 00:16:07 +0800

    Update app.php

--- app.php
+++ app.php
@@ -1,12 +1,8 @@
 <!DOCTYPE html>
 <html lang="zh-cn" ng-app="surmon">
   <meta charset="UTF-8">
-  <meta name="Author" Content="司马萌,surmon@qq.com">
-  <meta name="Copyright" Content="本页版权归Surmon.me所有,All Rights Reserved">
   <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name="viewport" >
-  <title>司马萌</title>
-  <meta name="description" itemprop="description" content="凡心所向，素履所往，生如逆旅，一苇以航。" />
-  <meta name="keywords" itemprop="keywords" content="司马萌,马赐崇,马谡,javascript,前端技术," />
+  <title>Surmon</title>
   <script src="<?php bloginfo('template_url'); ?>/app/angular/angular.js" ></script>
   <script src="<?php bloginfo('template_url'); ?>/app/angular/angular-route.js" ></script>
   <script src="<?php bloginfo('template_url'); ?>/app/app.js" ></script>
